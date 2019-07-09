e6eca0e42cb3a8c8eb2df6e7b8b042648a4d4888
progyu <rocket7101@gmail.com>
2019-07-09 23:56:00 +0900

    movie-modal-css

--- projectMegabox/src/app/shared/quick-booking/main-view/movie-modal/movie-modal.component.ts
+++ projectMegabox/src/app/shared/quick-booking/main-view/movie-modal/movie-modal.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-modal.component.scss']
 })
 export class MovieModalComponent implements OnInit {
-
+  movies = [
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+  ]
   constructor() { }
 
   ngOnInit() {
