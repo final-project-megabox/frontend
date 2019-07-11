5496498e4336ca219ffc284a32a6a51215f2ecef
goodsungjin <mirepach@naver.com>
2019-07-12 01:36:42 +0900

    add time carousel

--- projectMegabox/src/app/shared/quick-booking/main-view/main-view.component.ts
+++ projectMegabox/src/app/shared/quick-booking/main-view/main-view.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
   movieModal: false;
-
+  timeTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+  currentTime: number;
+  listLocation = 0;
   constructor() { }
 
   ngOnInit() {
+    this.currentTime = new Date().getHours();
   }
 
+  nextTime(ulTime: HTMLUListElement) {
+    if (this.listLocation === -616) return;
+
+    this.listLocation -= 44;
+    ulTime.style.transform = `translateX(${this.listLocation}px)`;
+  }
+
+  prevTime(ulTime: HTMLUListElement) {
+    if (this.listLocation === 0) return;
+
+    this.listLocation += 44;
+    ulTime.style.transform = `translateX(${this.listLocation}px)`;
+  }
 }
