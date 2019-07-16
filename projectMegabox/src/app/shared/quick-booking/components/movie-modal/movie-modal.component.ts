32d044f53267ba2a72be5bce89335562faac8c9f
progyu <rocket7101@gmail.com>
2019-07-16 15:34:30 +0900

    Bugfix: property access bug fixed.

--- projectMegabox/src/app/shared/quick-booking/components/movie-modal/movie-modal.component.ts
+++ projectMegabox/src/app/shared/quick-booking/components/movie-modal/movie-modal.component.ts
@@ -61,34 +61,35 @@ export class MovieModalComponent implements OnInit {
   ]
 
   // 포스터 클릭 시 새롭게 만들어질 배열, 영화의 이름이 들어감.
-  selectMoive:object[] = [];
+  selectMovie:object[] = [];
   selectPoster:HTMLElement[] = [];
 
   sortItems: SortItem[] = ['예매율순','가나다순','개봉일순'];
   sortState: SortItem = '예매율순';
 
-  // 포스터를 클릭 했을 때 selectMoive 배열 안에 추가하고 다시 또 클릭하면 selectMoive 배열에서 삭제 또 그에 따른 배경색 변화와 최대 4개까지 선택 가능 기능
+  // 포스터를 클릭 했을 때 selectMovie 배열 안에 추가하고 다시 또 클릭하면 selectMovie 배열에서 삭제 또 그에 따른 배경색 변화와 최대 4개까지 선택 가능 기능
   addPoster(movie: object ,poster: HTMLElement) {
-    if(poster.style.backgroundColor && this.selectMoive.length < 4) {
-      this.selectMoive = [...this.selectMoive, movie];
+    if(poster.style.backgroundColor && this.selectMovie.length < 4) {
+      this.selectMovie = [...this.selectMovie, movie];
       this.selectPoster = [...this.selectPoster, poster];
     } 
-    else if (!poster.style.backgroundColor || this.selectMoive.length > 3) {
-      this.selectMoive = this.selectMoive.filter(mov => mov !== movie);
+    else if (!poster.style.backgroundColor || this.selectMovie.length > 3) {
+      this.selectMovie = this.selectMovie.filter(mov => mov !== movie);
       poster.style.backgroundColor = null; 
       poster.lastElementChild.setAttribute('style', 'color: null');
     }
 
-    if(this.selectMoive.length === 4 && !poster.style.backgroundColor) {
+    if(this.selectMovie.length === 4 && !poster.style.backgroundColor) {
       alert('4개까지만 선택할 수 있습니다.');
     }
   }
 
-  // selectMoive 배열의 요소로 이루어진 포스터 위 글(?)의 x 버튼 클릭 시 selectMoive 배열에서 요소가 삭제되고 배경색이 변함.
+  // selectMovie 배열의 요소로 이루어진 포스터 위 글(?)의 x 버튼 클릭 시 selectMovie 배열에서 요소가 삭제되고 배경색이 변함.
   deleteMovie(selected: object) {
-    this.selectMoive = this.selectMoive.filter(mov => mov !== selected);
+    this.selectMovie = this.selectMovie.filter(mov => mov !== selected);
+    
     this.selectPoster.forEach(li => {
-      if(+li.id === selected.id) {
+      if(+li.id === selected["id"]) {
         li.style.backgroundColor = null;
         li.lastElementChild.setAttribute('style', 'color: null');
       }
