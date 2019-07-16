dc48d0bd2b238f1842a365d596b29264c59febba
progyu <rocket7101@gmail.com>
2019-07-16 14:51:23 +0900

    Change logic, add cursor: pointer

--- projectMegabox/src/app/shared/quick-booking/components/movie-modal/movie-modal.component.ts
+++ projectMegabox/src/app/shared/quick-booking/components/movie-modal/movie-modal.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { SortItem } from '../../models/sortItem.type';
+import { Component, OnInit } from '@angular/core';
+
 import { QuickBookingService } from '../../service/quick-booking.service';
+
+import { SortItem } from '../../models/sortItem.type';
 
 @Component({
   selector: 'app-movie-modal',
@@ -10,6 +12,8 @@ import { QuickBookingService } from '../../service/quick-booking.service';
 
 export class MovieModalComponent implements OnInit {
   constructor(private bookingService: QuickBookingService) { }
+
+  ngOnInit() {}
   
   movies = [
     {id: 0, age: 'age-12', name: '스파이더맨: 파 프롬 홈', reservationRate: '90', releaseDate: '2019-07-03'
@@ -57,18 +61,20 @@ export class MovieModalComponent implements OnInit {
   ]
 
   // 포스터 클릭 시 새롭게 만들어질 배열, 영화의 이름이 들어감.
-  selectMoive:string[] = [];
+  selectMoive:object[] = [];
+  selectPoster:HTMLElement[] = [];
 
   sortItems: SortItem[] = ['예매율순','가나다순','개봉일순'];
   sortState: SortItem = '예매율순';
 
   // 포스터를 클릭 했을 때 selectMoive 배열 안에 추가하고 다시 또 클릭하면 selectMoive 배열에서 삭제 또 그에 따른 배경색 변화와 최대 4개까지 선택 가능 기능
-  addPoster(movie: string, poster: HTMLElement) {
+  addPoster(movie: object ,poster: HTMLElement) {
     if(poster.style.backgroundColor && this.selectMoive.length < 4) {
       this.selectMoive = [...this.selectMoive, movie];
+      this.selectPoster = [...this.selectPoster, poster];
     } 
     else if (!poster.style.backgroundColor || this.selectMoive.length > 3) {
-      this.selectMoive = this.selectMoive.filter((name) => name !== movie);
+      this.selectMoive = this.selectMoive.filter(mov => mov !== movie);
       poster.style.backgroundColor = null; 
       poster.lastElementChild.setAttribute('style', 'color: null');
     }
@@ -79,17 +85,13 @@ export class MovieModalComponent implements OnInit {
   }
 
   // selectMoive 배열의 요소로 이루어진 포스터 위 글(?)의 x 버튼 클릭 시 selectMoive 배열에서 요소가 삭제되고 배경색이 변함.
-  deleteMovie(selected: string) {
-    this.selectMoive = this.selectMoive.filter((name) => name !== selected);
-    const elems = document.querySelectorAll('.blind');
-    [...elems].forEach(span => {
-      if(span.textContent === selected) {
-        span.parentNode.parentNode.style.backgroundColor = null
-        span.parentNode.nextElementSibling.nextElementSibling.style.color = null
+  deleteMovie(selected: object) {
+    this.selectMoive = this.selectMoive.filter(mov => mov !== selected);
+    this.selectPoster.forEach(li => {
+      if(+li.id === selected.id) {
+        li.style.backgroundColor = null;
+        li.lastElementChild.setAttribute('style', 'color: null');
       }
-    });
-  }
-
-  ngOnInit() {
+    })
   }
 }
