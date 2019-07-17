2655dea7877bc3c82e49c0c83a8fccec319bd878
progyu <rocket7101@gmail.com>
2019-07-17 17:41:47 +0900

    add: regions.type.ts

--- projectMegabox/src/app/shared/quick-booking/components/theater-modal/theater-modal.component.ts
+++ projectMegabox/src/app/shared/quick-booking/components/theater-modal/theater-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { QuickBookingService } from '../../service/quick-booking.service';
 
-type regions = '선호 영화관' | '서울' | '경기' | '부산' ;
+import { regions } from '../../models/regions.type';
 
 @Component({
   selector: 'app-theater-modal',
@@ -11,10 +11,9 @@ type regions = '선호 영화관' | '서울' | '경기' | '부산' ;
 })
 export class TheaterModalComponent implements OnInit {
 
-  constructor(private bookingService: QuickBookingService) { }
+  constructor(private bookingService: QuickBookingService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   depthOneState: regions = '선호 영화관';
 
@@ -29,30 +28,8 @@ export class TheaterModalComponent implements OnInit {
     { id: 3, name: '고양스타필드', city: '경기' }, 
     { id: 4, name: '해운대(장산)', city: '부산' }, 
   ];
-  
-  // navItems = [
-  //   {
-  //     id: 0,
-  //     name: '서울',
-  //     region: ['강남', '신촌', '코엑스'],
-  //     map: 'url'
-  //   },
-  //   {
-  //     id: 1,
-  //     name: '경기',
-  //     region: ['고양스타필드'],
-  //     map: 'url'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: '부산',
-  //     region: ['해운대(장산)'],
-  //     map: 'url'
-  //   },
-  //   ['강남', '신촌', '코엑스', '고양스타필드', '해운대(장산)' ]
-  // ]
 
-  enter(area: string) {
-    this.hoverRegion = area;
+  hoverArea(detailArea: string) {
+    this.hoverRegion = detailArea;
   }
 }
