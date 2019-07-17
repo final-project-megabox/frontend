81e340eb2ec17aa729d438f3eeaf00d79cadd7ca
goodsungjin <mirepach@naver.com>
2019-07-17 16:56:08 +0900

    feat: edit my page routing

--- projectMegabox/src/app/app-routing.module.ts
+++ projectMegabox/src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { MyPageComponent } from './components/my-page/my-page.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
+import { MypageMainComponent } from './components/my-page/mypage-main/mypage-main.component';
+import { MypageBookingComponent } from './components/my-page/mypage-booking/mypage-booking.component';
+import { MypageMoviestoryComponent } from './components/my-page/mypage-moviestory/mypage-moviestory.component';
+
+
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: '', pathMatch: 'full', redirectTo: 'home',},
   { path: 'home', component: MainPageComponent},
-  { path: 'mypage', component: MyPageComponent},
+  { path: 'mypage', component: MypageMainComponent,
+    children: [
+      { path: 'booking', component: MypageBookingComponent },
+      { path: 'my-movie-story', component: MypageMoviestoryComponent },
+  ]},
   { path: '**', component: NotFoundComponent},
 
 ];
--- projectMegabox/src/app/components/my-page/my-page.module.ts
+++ projectMegabox/src/app/components/my-page/my-page.module.ts
@@ -1,15 +1,12 @@
-
-import { MypageMoviestoryComponent } from './mypage-moviestory/mypage-moviestory.component';
-
-import { MypageBookingComponent } from './mypage-booking/mypage-booking.component';
-import { MypageNaviComponent } from './mypage-navi/mypage-navi.component';
-import { MypageMainComponent } from './mypage-main/mypage-main.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { MyPageComponent } from './my-page.component';
-import { MyPageRoutingModule } from './mypage-routing.module';
+import { MypageMoviestoryComponent } from './mypage-moviestory/mypage-moviestory.component';
+import { MypageBookingComponent } from './mypage-booking/mypage-booking.component';
+import { MypageNaviComponent } from './mypage-navi/mypage-navi.component';
+import { MypageMainComponent } from './mypage-main/mypage-main.component';
+
 
 @NgModule({
   declarations: [
@@ -21,8 +18,7 @@ import { MyPageRoutingModule } from './mypage-routing.module';
 
   ],
   imports: [
-    CommonModule,
-    MyPageRoutingModule
+    CommonModule
   ]
 })
 export class MyPageModule { }
