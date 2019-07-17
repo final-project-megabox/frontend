97358ea56006bc927e43f18accdbde1f912e4bc8
goodsungjin <mirepach@naver.com>
2019-07-17 20:23:50 +0900

    feat: Add days carousel

--- projectMegabox/src/app/shared/quick-booking/components/main-view/main-view.component.ts
+++ projectMegabox/src/app/shared/quick-booking/components/main-view/main-view.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { QuickBookingService } from '../../service/quick-booking.service';
-import { RootService } from 'src/app/core/service/root.service';
 
 @Component({
   selector: 'app-main-view',
@@ -11,11 +10,14 @@ export class MainViewComponent implements OnInit {
   timeTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
   currentTime: number;
   listLocation = 0;
+  afterToday = [];
 
-  constructor(private bookingService: QuickBookingService) { }
-
+  constructor(private bookingService: QuickBookingService) {}
+  
   ngOnInit() {
     this.currentTime = new Date().getHours();
+    this.afterTodayMonth();
+    console.log(this.afterToday)
   }
 
   nextTime(ulTime: HTMLUListElement) {
@@ -33,4 +35,32 @@ export class MainViewComponent implements OnInit {
   }
 
 
+  nextDay(ulTime: HTMLUListElement) {
+    if (this.listLocation === -(this.afterToday.length - 5) * 68) return;
+
+    this.listLocation -= 68;
+    ulTime.style.transform = `translateX(${this.listLocation}px)`;
+  }
+
+  prevDay(ulTime: HTMLUListElement) {
+    if (this.listLocation === 0) return;
+
+    this.listLocation += 68;
+    ulTime.style.transform = `translateX(${this.listLocation}px)`;
+  }
+
+  checkDayActive(day: string, elem?: HTMLUListElement) {
+    this.bookingService.activeToday = day;
+    
+    if (this.bookingService.today === day) return;
+
+    const idx = this.afterToday.findIndex(item => item.fullDay === day);
+    this.listLocation = idx * -68;
+    
+    elem.style.transform = `translateX(${this.listLocation}px)`;
+  }
+
+  afterTodayMonth() {
+    this.afterToday = [...this.bookingService.calDay(), ...this.bookingService.nextMonth];
+  }
 }
--- projectMegabox/src/app/shared/quick-booking/service/quick-booking.service.ts
+++ projectMegabox/src/app/shared/quick-booking/service/quick-booking.service.ts
@@ -1,10 +1,117 @@
 import { HttpClient } from '@angular/common/http';
+import { Days } from '../models/days.interface';
 
 export class QuickBookingService {
   celendarModalState = false;
   movieModalState = false;
   theaterModalState = false;
-
+  
+  
+  date = new Date();
+  year = this.date.getFullYear();
+  _month = this.date.getMonth();
+  today = `${this.date.getFullYear()}${this.date.getMonth()}${this.date.getDate()}`;
+  activeToday = this.today;
+  nextMonth: Days[];
+  
+  thisDays: Days[];
+  
+  weeks = [[], [], [], [], [], []];
+  
   constructor(private http: HttpClient) { }
+  
+  ngOnInit() {}
 
+
+  // this.month = a
+  set month(value: number) {
+
+    this._month = value;
+    this.calDay();
+    this.splitDay();
+  }
+
+  // a = this.month
+  get month() {
+    return this._month;
+  }
+
+  calDay() {
+    this.weeks = [[], [], [], [], [], []];
+
+    let lastDay = new Date(this.year, this.month + 1, 0).getDate();
+
+    let thisDays = [];
+
+    for (let i = 1; i <= 30 - this.date.getDate(); i++) {
+      let thisDate = new Date(this.year, this.month + 1, i);
+
+      this.nextMonth = thisDays.length ? [...this.nextMonth, { day: thisDate.getDate(), date: this.translate(thisDate.getDay()), fullDay: `${thisDate.getFullYear()}${thisDate.getMonth()}${thisDate.getDate()}` }] : thisDays = [{ day: thisDate.getDate(), date: this.translate(thisDate.getDay()), fullDay: `${thisDate.getFullYear()}${thisDate.getMonth()}${thisDate.getDate()}` }];
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+      let thisDate = new Date(this.year, this.month, i);
+
+      thisDays = thisDays.length ? [...thisDays, { day: thisDate.getDate(), date: this.translate(thisDate.getDay()), fullDay: `${thisDate.getFullYear()}${thisDate.getMonth()}${thisDate.getDate()}` }] : thisDays = [{ day: thisDate.getDate(), date: this.translate(thisDate.getDay()), fullDay: `${thisDate.getFullYear()}${thisDate.getMonth()}${thisDate.getDate()}` }];
+    }
+
+    this.thisDays = thisDays;
+
+    return this.findToday(thisDays);
+  }
+
+  splitDay() {
+    let firstSat = this.thisDays.findIndex(item => item.date === '토');
+    let stopIdx = 0;
+
+    for (let i = 0; i <= 5; i++) {
+      if (i > 0) stopIdx += 1;
+
+      this.thisDays.some((item, idx) => {
+        if (stopIdx <= firstSat && idx >= stopIdx) {
+          this.weeks[i] = [...this.weeks[i], item];
+
+          stopIdx = idx;
+
+          return stopIdx === firstSat;
+        }
+
+      })
+      firstSat += 7;
+    }
+    this.weeks = this.weeks.filter(item => item.length)
+    
+    if (this.weeks[0].length === 7) return;
+
+    const empty = 7 - this.weeks[0].length;
+
+    for (let i = 0; i < empty; i++) {
+      this.weeks[0] = [{}, ...this.weeks[0]]
+    }
+
+  }
+
+
+  translate(number: number) {
+    let date: string;
+
+    if (number === 0) date = "일";
+    if (number === 1) date = "월";
+    if (number === 2) date = "화";
+    if (number === 3) date = "수";
+    if (number === 4) date = "목";
+    if (number === 5) date = "금";
+    if (number === 6) date = "토";
+
+    return date;
+  }
+
+
+
+
+  findToday(days: any) {
+    const today = days.findIndex(day => day.fullDay === this.today);
+
+    return days.slice(today);
+  }
 }
