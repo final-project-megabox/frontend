9eb61100514e797784106cd8dd9452eca7f1b5f1
immsee098 <immsee098@gmail.com>
2019-07-17 16:26:44 +0900

    ...

---
+++ projectMegabox/src/app/components/my-page/mypage-main/mypage-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mypage-main',
+  templateUrl: './mypage-main.component.html',
+  styleUrls: ['./mypage-main.component.scss']
+})
+export class MypageMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
