3510aae4e4dc1356c8d4b98099e9935e26320dc7
progyu <rocket7101@gmail.com>
2019-07-17 17:24:44 +0900

    feat: mouse event in map

--- projectMegabox/src/app/shared/quick-booking/components/theater-modal/theater-modal.component.ts
+++ projectMegabox/src/app/shared/quick-booking/components/theater-modal/theater-modal.component.ts
@@ -11,9 +11,16 @@ type regions = '선호 영화관' | '서울' | '경기' | '부산' ;
 })
 export class TheaterModalComponent implements OnInit {
 
+  constructor(private bookingService: QuickBookingService) { }
+
+  ngOnInit() {
+  }
+
   depthOneState: regions = '선호 영화관';
 
   selectRegions: regions[] = ['선호 영화관','서울', '경기', '부산'];
+
+  hoverRegion: string;
 
   detailRegions = [
     { id: 0, name: '강남', city: '서울' }, 
@@ -45,9 +52,7 @@ export class TheaterModalComponent implements OnInit {
   //   ['강남', '신촌', '코엑스', '고양스타필드', '해운대(장산)' ]
   // ]
 
-  constructor(private bookingService: QuickBookingService) { }
-
-  ngOnInit() {
+  enter(area: string) {
+    this.hoverRegion = area;
   }
-
 }
