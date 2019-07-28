e8160803746cba75255a715522d3a0a0de1869d2
immsee098 <immsee098@gmail.com>
2019-07-29 04:33:14 +0900

    1

--- projectMegabox/src/app/components/my-page/mypage-main/mypage-main.component.ts
+++ projectMegabox/src/app/components/my-page/mypage-main/mypage-main.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Userinfos {
+  email: string;
+  name: string;
+  password: string;
+  birthDate: string;
+  phoneNumber: number;
+  preferTheater: string;
+  watchedMovie: string;
+  wishMovie: string;
+}
+
 @Component({
   selector: 'app-mypage-main',
   templateUrl: './mypage-main.component.html',
@@ -9,9 +21,22 @@ export class MypageMainComponent implements OnInit {
 
   navState = '공지사항';
 
+  numBack: number;
+
+
+  userinfos: Userinfos[] = [
+    // tslint:disable-next-line: max-line-length
+    { email: 'immsee098@gmail.com', name: '윤해서', password: 'qwerty123', birthDate: '1995-04-22', phoneNumber: 26057621, preferTheater: '상봉, 코엑스', watchedMovie: '라이온킹킹킹키이킹', wishMovie: '뭐하지'},
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
+    const info = [...this.userinfos]
+    const splitNum = (info[0].phoneNumber + '').slice(4, 8);
+    const stringTonum = parseInt(splitNum);
+    this.numBack = stringTonum;
   }
 
   tabChange(text) {
@@ -19,5 +44,13 @@ export class MypageMainComponent implements OnInit {
     this.navState = text;
     console.log(this.navState);
   }
+
+  // phoneNumcut() {
+  //   const info = [...this.userinfos]
+  //   const splitNum = (info[0].phoneNumber + '').slice(4, 8);
+  //   const stringTonum = parseInt(splitNum);
+  //   this.numBack = stringTonum;
+  // }
+
 
 }
