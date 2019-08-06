dafd707ef110e8242d8e2a9f8ebda224b93a891f
goodsungjin <mirepach@naver.com>
2019-08-06 17:40:17 +0900

    feat: add seat 140

---
+++ projectMegabox/src/app/shared/quick-booking/components/seat-selection-modal/components/seat-one-hundred-forty/seat-one-hundred-forty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-one-hundred-forty',
+  templateUrl: './seat-one-hundred-forty.component.html',
+  styleUrls: ['./seat-one-hundred-forty.component.scss']
+})
+export class SeatOneHundredFortyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projectMegabox/src/app/shared/quick-booking/components/seat-selection-modal/seat.module.ts
+++ projectMegabox/src/app/shared/quick-booking/components/seat-selection-modal/seat.module.ts
@@ -5,11 +5,13 @@ import { SeatService } from './components/service/seat.service';
 
 import { SeatSelectionModalComponent } from './seat-selection-modal.component';
 import { SeatOneHundredThirtyComponent } from './components/seat-one-hundred-thirty/seat-one-hundred-thirty.component';
+import { SeatOneHundredFortyComponent } from './components/seat-one-hundred-forty/seat-one-hundred-forty.component';
 
 @NgModule({
   declarations: [
     SeatSelectionModalComponent,
-    SeatOneHundredThirtyComponent
+    SeatOneHundredThirtyComponent,
+    SeatOneHundredFortyComponent
   ],
   imports: [
     CommonModule,
