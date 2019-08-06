db7e1bce676ed7100d1f5307e37cd570b18c1e8a
cool232000 <cool232000@gmail.com>
2019-08-07 01:25:25 +0900

    connect quick booking page

--- projectMegabox/src/app/components/main-page/movie-rank/movie-detail/movie-detail.component.ts
+++ projectMegabox/src/app/components/main-page/movie-rank/movie-detail/movie-detail.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MovieDetailService } from '../service/movie-detail.service';
-import { Movies } from 'src/app/shared/quick-booking/models/movies.interface';
-import { QuickBookingService } from 'src/app/shared/quick-booking/service/quick-booking.service';
+
 import { RootService } from 'src/app/core/service/root.service';
+import { MovieDetailService } from '../service/movie-detail.service';
+import { QuickBookingService } from 'src/app/shared/quick-booking/service/quick-booking.service';
+
 import { MovieDetail } from '../models/rank-movie-interface';
+import { Movies } from 'src/app/shared/quick-booking/models/movies.interface';
+
 
 @Component({
   selector: 'app-movie-detail',
@@ -13,11 +16,12 @@ import { MovieDetail } from '../models/rank-movie-interface';
 export class MovieDetailComponent implements OnInit {
 
   @Input() detailMovie: MovieDetail;
-  // rankovie: Movies[];
-  rankMovie = this.rankService;
 
   movies: Movies[];
-
+  
+  rankMovie = this.rankService.movies;
+  selectMovie = this.rankService.selectMovie;
+  
   tipClick = false;
 
   constructor(
@@ -30,17 +34,11 @@ export class MovieDetailComponent implements OnInit {
   }
   
   ngOnChanges() {
-    console.log(this.detailMovie);
-    
   }
   
   closeDetail() {
     this.movieDetailService.detailModalState = false;
   }
-
-  // openDetail() {
-  //   this.movieDetailService.detailModalState = true;
-  // }
 
   descending(key: string) {
     return function(a: Movies, b: Movies) {
@@ -48,13 +46,12 @@ export class MovieDetailComponent implements OnInit {
     }
   }
 
-  ratingNow(rankmovie: Movies) {
-    console.log(rankmovie);
-    return this.rankMovie.movies.sort(this.descending('booking_rate')).findIndex(i => i['booking_rate'] === rankmovie['booking_rate'])+1;
+  ratingNow(detailMovie) {
+    return this.rankMovie.sort(this.descending('booking_rate')).findIndex(i => i['booking_rate'] === detailMovie['booking_rate'])+1;
   }
 
-  reservationMovie(rankmovie: Movies) {
-    this.rankService.selectMovie = [rankmovie];
+  reservationMovie(detailSelect) {
+    this.rankService.selectMovie = [detailSelect];
     this.rootService.quickBookingModalState = true;
   }
 
