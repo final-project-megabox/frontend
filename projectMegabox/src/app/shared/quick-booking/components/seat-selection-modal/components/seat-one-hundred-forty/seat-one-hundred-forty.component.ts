e8e3624316a9e2e94a93772d7538618b29453222
goodsungjin <mirepach@naver.com>
2019-08-06 17:51:58 +0900

    feat: edit seat 140

--- projectMegabox/src/app/shared/quick-booking/components/seat-selection-modal/components/seat-one-hundred-forty/seat-one-hundred-forty.component.ts
+++ projectMegabox/src/app/shared/quick-booking/components/seat-selection-modal/components/seat-one-hundred-forty/seat-one-hundred-forty.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeatService } from '../service/seat.service';
 
 @Component({
   selector: 'app-seat-one-hundred-forty',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seat-one-hundred-forty.component.scss']
 })
 export class SeatOneHundredFortyComponent implements OnInit {
-
-  constructor() { }
+  allSeat = [];
+  constructor(private seatService: SeatService) { }
 
   ngOnInit() {
+    this.createSeat();
   }
 
+  createSeat() {
+    let ascii = 65;
+    let currentAsc: string;
+
+    for(let i = 0; i < 9; i++) {
+      this.allSeat = [...this.allSeat, []];
+
+      if (i < 2) {
+        for(let j = 0; j < 15; j++) {
+          if (j === 0) currentAsc = String.fromCharCode(ascii);
+
+          this.allSeat[i] = [ ...this.allSeat[i], 
+            { 
+              class: j === 0 ? 'title' : 'value',
+              postSeat: j === 0 ? 0 : `${currentAsc}${j}`,
+              seatNumber: j === 0 ? String.fromCharCode(ascii) : j 
+            }
+          ];
+        }
+        ascii += 1;
+      } else {
+        for(let j = 0; j < 17; j++) {
+          if (j === 0) currentAsc = String.fromCharCode(ascii);
+
+          this.allSeat[i] = [ ...this.allSeat[i], 
+            { 
+              class: j === 0 ? 'title' : 'value',
+              postSeat: j === 0 ? 0 : `${currentAsc}${j}`,
+              seatNumber: j === 0 ? String.fromCharCode(ascii) : j 
+            }
+          ];
+        }
+        ascii += 1;
+      }
+    }
+  }
+
+  click(value: string) {
+    console.log(value)
+    console.log(this.seatService.normal, this.seatService.youth, this.seatService.favor);
+  }
 }
