178f9417d452672a76b393a359b4846be6d846c0
goodsungjin <mirepach@naver.com>
2019-08-07 13:03:44 +0900

    edit quick booking modal state

--- projectMegabox/src/app/core/service/root.service.ts
+++ projectMegabox/src/app/core/service/root.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RootService {
-  quickBookingModalState = true;
+  quickBookingModalState = false;
   loginModalState = false;
   myPageModal = false;
   requiredLogin = false;
